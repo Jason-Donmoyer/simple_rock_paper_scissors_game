73e49d0f551f7e4d05a1ca671d3eb81212b1ea13
Jason-Donmoyer <j.donmoyer523@gmail.com>
2020-01-05 10:21:48 -0500

    added scoring and replay option

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,46 +1,68 @@
 # Rock Paper Scissors Game
 player1_score = 0
 player2_score = 0
+game = True
+round = 1
 
+while game:
 
-print('.....ROCK.....')
-print('....PAPER.....')
-print('...SCISSORS...')
-print('..............')
+    print(f'ROUND {round}')
+    print('.....ROCK.....')
+    print('....PAPER.....')
+    print('...SCISSORS...')
+    print('..............')
 
-#player1 input
-player1_input = input('SHOOT!  -> ')
-#player2 input
-player2_input = input('SHOOT!  -> ')
-#result
+    #player1 input
+    player1_input = input('SHOOT!  -> ')
+    if player1_input != 'rock':
+        if player1_input != 'paper':
+            if player1_input != 'scissors':
+                print('Please enter a valid input!!!')
+                player1_input = input('SHOOT!  -> ')
+    #player2 input
+    player2_input = input('SHOOT!  -> ')
+    if player2_input != 'rock':
+        if player2_input != 'paper':
+            if player2_input != 'scissors':
+                print('Please enter a valid input!!!')
+                player2_input = input('SHOOT!  -> ')
+    #result
 
-if player1_input == 'rock':
-    if player2_input == 'rock':
-        print('draw')
-    elif player2_input == 'paper':
-        print('paper covers rock')
-        player2_score += 1
-    elif player2_input == 'scissors':
-        print('rock smashes scissors')
-        player1_score += 1
-elif player1_input == 'paper':
-    if player2_input == 'paper':
-        print('draw')
-    elif player2_input == 'rock':
-        print('paper covers rock')
-        player1_score += 1
-    elif player2_input == 'scissors':
-        print('scissors cut paper')
-        player2_score += 1
-elif player1_input == 'scissors':
-    if player2_input == 'scissors':
-        print('draw')
-    elif player2_input == 'rock':
-        print('rock smashes scissors')
-        player2_score += 1
-    elif player2_input == 'paper':
-        print('scissors cut paper')
-        player1_score += 1
+    if player1_input == 'rock':
+        if player2_input == 'rock':
+            print('draw')
+        elif player2_input == 'paper':
+            print('paper covers rock')
+            player2_score += 1
+        elif player2_input == 'scissors':
+            print('rock smashes scissors')
+            player1_score += 1
+    elif player1_input == 'paper':
+        if player2_input == 'paper':
+            print('draw')
+        elif player2_input == 'rock':
+            print('paper covers rock')
+            player1_score += 1
+        elif player2_input == 'scissors':
+            print('scissors cut paper')
+            player2_score += 1
+    elif player1_input == 'scissors':
+        if player2_input == 'scissors':
+            print('draw')
+        elif player2_input == 'rock':
+            print('rock smashes scissors')
+            player2_score += 1
+        elif player2_input == 'paper':
+            print('scissors cut paper')
+            player1_score += 1
 
-print(f'Player1 score: {player1_score}')
-print(f'Player2 score: {player2_score}')
+    print(f'Player1 score: {player1_score}')
+    print(f'Player2 score: {player2_score}')
+
+    playAgain = input("Play again? Y/N")
+    if playAgain == 'Y':
+        game = True
+        round += 1
+    else:
+        game = False
+
